74d4540a2ce559a453fa64b296d1a8511a943485
AnhHoangQuach <hoanganh36.work@gmail.com>
2023-02-09 11:58:27 +0700

    feat: complete erc20

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,15 @@
 require('@nomicfoundation/hardhat-toolbox')
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
 module.exports = {
   solidity: '0.8.17',
+  networks: {
+    bsctest: {
+      url: 'https://data-seed-prebsc-2-s2.binance.org:8545',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 }
